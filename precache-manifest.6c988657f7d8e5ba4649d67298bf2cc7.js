3e1099ca48e21ab340f8756f022acb513f277325
Gurjot Singh <gurjot@myerrunds.com>
2019-06-10 21:21:39 -0400

    Updates

---
+++ precache-manifest.6c988657f7d8e5ba4649d67298bf2cc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1accdca2f871253912b6ab90c679e7",
+    "url": "/gurjotsingh.github.io/index.html"
+  },
+  {
+    "revision": "218d1e7fd3509efe478c",
+    "url": "/gurjotsingh.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gurjotsingh.github.io/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "218d1e7fd3509efe478c",
+    "url": "/gurjotsingh.github.io/static/js/main.7dd6dffa.chunk.js"
+  },
+  {
+    "revision": "c2074f940f10129dbcef",
+    "url": "/gurjotsingh.github.io/static/js/runtime~main.f0197373.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gurjotsingh.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
